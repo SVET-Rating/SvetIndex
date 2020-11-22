022e18ab43e5bc85567c95c832de31efb1d67e33
stanta <st.takt@gmail.com>
2020-11-22 14:44:59 +0300

    adding ethvtx_embark

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store'
+//import store from './redux/store'
+import { createVtxStore } from '../ethvtx_config/createVtxStore';
+import { setupWeb3 } from '../ethvtx_config/setupWeb3';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+
+const main = async () => {
+
+  const store = createVtxStore();
+  await setupWeb3(store);
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+};
+
+main();
 
