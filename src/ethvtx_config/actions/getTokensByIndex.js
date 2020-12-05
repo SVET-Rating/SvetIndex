aec234b8a84b50ee0206a3c9a107331c4f73917c
stanta <st.takt@gmail.com>
2020-12-05 11:09:44 +0300

    fixing getTokensByIndex activeslist

--- src/ethvtx_config/actions/getTokensByIndex.js
+++ src/ethvtx_config/actions/getTokensByIndex.js
@@ -1,17 +1,31 @@
 import {GET_TOKENS_BY_INDEX} from './types';
-import { getContract, getContractList } from 'ethvtx/lib/contracts/helpers/getters';
+import { getContract } from 'ethvtx/lib/contracts/helpers/getters';
 
 const getTokensByIndex = (address) => {
     return (dispatch, getState) => {
         // grab current state
         const state = getState();
-    return {
-        type: GET_TOKENS_BY_INDEX,
-        payload: getContract(state, 'IndexToken', address).fn.getActivesList()
-        // payload: [{name:'Good invest',symbol:'GI',amount:23},
-        //           {name:'Good invest1',symbol:'GI1',amount:21},
-        //           {name:'Good invest2',symbol:'GI2',amount:22}]
+        const curTokenContr = getContract(state, 'IndexToken', address);
+        if (curTokenContr !== undefined ) {
+            var actives = []; 
+            curTokenContr.fn.getActivesList().map((item, key) => {
+                const curERCtok = getContract(state, 'IndexToken', item.addrActive);
+                var curActive;
+                curActive.name =curERCtok.fn.name();
+                curActive.symbol =curERCtok.fn.symbol();
+                curActive.address = item.addrActive;
+                curActive.amount = item.amount;
+                actives.push(curActive);
+            });
+                return {
+                    type: GET_TOKENS_BY_INDEX,
+                    payload: actives
+                    // payload: [{name:'Good invest',symbol:'GI',amount:23},
+                    //           {name:'Good invest1',symbol:'GI1',amount:21},
+                    //           {name:'Good invest2',symbol:'GI2',amount:22}]
+                };
+        }
     }
-}}
+}
 
 export default getTokensByIndex;
