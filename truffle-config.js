13508e7f15eed1defee26d6e96ee912e961ac505
stanta <st.takt@gmail.com>
2021-04-12 22:16:58 +0300

    tested & fixed merge mistakes

--- truffle-config.js
+++ truffle-config.js
@@ -23,13 +23,13 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
-
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "src/abi"),
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
    * run `develop` or `test`. You can ask a truffle command to use a specific
    * network from the command line, e.g
    *
