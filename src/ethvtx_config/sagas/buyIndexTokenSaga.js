95f813b4f05de76782e74c06ed9330c2a3ff53bc
Vital Raicheu <rvitaly1978@gmail.com>
2021-06-25 20:48:02 +0300

    fix: fix async/await in sagas, add culc wei in gwei with web3.utils.fromWei

--- src/ethvtx_config/sagas/buyIndexTokenSaga.js
+++ src/ethvtx_config/sagas/buyIndexTokenSaga.js
@@ -1,68 +1,53 @@
-import { call, take, put, fork, takeEvery, cancelled, race } from 'redux-saga/effects'
-import { BUY_INDEX_TOKENS,
-         BUY_INDEX_START_APPROVE,
-         BUY_INDEX_APPROVED,
-         BUY_INDEX_TRX_START,
-         BUY_INDEX_TRX_PROCESSED,BUY_INDEX_TRX_END,
-         BUY_INDEX_TRX_FAILED,
-} from '../actions/types'
+import { call, put, takeEvery } from 'redux-saga/effects';
+import {
+  BUY_INDEX_TOKENS,
+  BUY_INDEX_START_APPROVE,
+  BUY_INDEX_APPROVED,
+  BUY_INDEX_TRX_START,
+  BUY_INDEX_TRX_PROCESSED,
+  BUY_INDEX_TRX_END,
+  BUY_INDEX_TRX_FAILED,
+} from '../actions/types';
 
 const APPROVE_ERROR_MSG = 'Error with approve process';
 const PROCESS_ERROR_MSG = 'Error with buy process';
 
 const approveIndexBuyProcess = async (payload) => {
-    let amount_in_wei = await web3.utils.toWei(payload.ITAmount);
-    try {
-        const hash = await payload.svetToken._contract.methods
-            .approve( payload.ITokContract._address, amount_in_wei)
-            .send({from: payload.currentAddress})
-
-      // Если может вернуться не валидный 'hash',
-      // то здесь нужно выполнить проверку и выкинуть ошибку ('throw new Error()')
-        console.log(hash);
-        return hash
-    } catch {
-      throw new Error(APPROVE_ERROR_MSG);
-    }
-}
+  const amount_in_wei = web3.utils.toWei(payload.ITAmount);
+  try {
+    return (await payload.svetToken._contract.methods
+      .approve(payload.ITokContract._address, amount_in_wei)
+      .send({ from: payload.currentAddress }));
+  } catch {
+    throw new Error(APPROVE_ERROR_MSG);
+  }
+};
 
 const buyIndexTokenProcess = async (payload) => {
-    let amount_in_wei = await web3.utils.toWei(payload.ITAmount);
-    try {
-      const hash = payload.ITokContract._contract.methods
-                .buyIndexforSvetEth(amount_in_wei, payload.ITAddress, "600", "100")
-                .send({from: payload.currentAddress})
-
-        // Если может вернуться не валидный 'hash',
-        // то здесь нужно выполнить проверку и выкинуть ошибку ('throw new Error()')
-        console.log(hash);
-        return hash
-    } catch {
-      throw new Error(PROCESS_ERROR_MSG);
-    }
-}
+  const amount_in_wei = web3.utils.toWei(payload.ITAmount);
+  try {
+    return (await payload.ITokContract._contract.methods
+      .buyIndexforSvetEth(amount_in_wei, payload.ITAddress, "600", "100")
+      .send({ from: payload.currentAddress }));
+  } catch {
+    throw new Error(PROCESS_ERROR_MSG);
+  }
+};
 
 function* workerBuyIndexToken({ payload }) {
-    try {
-        yield put({type: BUY_INDEX_START_APPROVE})
-        const ahash = yield call(approveIndexBuyProcess, payload)
-        yield put({type: BUY_INDEX_APPROVED,payload:{approve_hash:ahash.transactionHash}})
-        yield put({type: BUY_INDEX_TRX_START})
-        const bhash = yield call(buyIndexTokenProcess, payload)
-        yield put({type: BUY_INDEX_TRX_PROCESSED, payload:{buyindex_hash:bhash.transactionHash}})
-        yield put({type: BUY_INDEX_TRX_END})
-    } catch (e) {
-      // yield put({type: BUY_INDEX_TRX_END})
-      yield put({ type: BUY_INDEX_TRX_FAILED, payload: { hasError: e.message } });
-    }
+  try {
+    yield put({ type: BUY_INDEX_START_APPROVE });
+    const ahash = yield call(approveIndexBuyProcess, payload);
+    yield put({ type: BUY_INDEX_APPROVED, payload: { approve_hash: ahash.transactionHash }});
+    yield put({ type: BUY_INDEX_TRX_START });
+    const bhash = yield call(buyIndexTokenProcess, payload);
+    yield put({ type: BUY_INDEX_TRX_PROCESSED, payload: { buyindex_hash: bhash.transactionHash }});
+    yield put({ type: BUY_INDEX_TRX_END });
+  } catch (e) {
+    yield put({ type: BUY_INDEX_TRX_FAILED, payload: { hasError: e.message }});
+  }
 }
 
-
 export function* watchIndexTokenBuyProcess() {
-    // console.log(payload)
-    //const { payload } = yield take(BUY_INDEX_TOKENS, workerBuyIndexToken)
-    //yield call(workerBuyIndexToken, payload)
-    yield takeEvery(BUY_INDEX_TOKENS, workerBuyIndexToken)
-
-    //yield cancel(workerBuyIndexToken)
+  yield takeEvery(BUY_INDEX_TOKENS, workerBuyIndexToken);
 }
--- src/ethvtx_config/sagas/getGasPrice.js
+++ src/ethvtx_config/sagas/getGasPrice.js
@@ -1,49 +1,44 @@
-import { call, take, put, fork, takeEvery, cancelled } from 'redux-saga/effects'
-import {START_INVEST,
-        START_INVEST_PROCESS,
-        GET_GAS_PRICE,
-        START_SELL_INDEX_PROCESS,
-        START_SELL_INDEX,
-        BUY_INDEX_TRX_END,
-        BUY_INDEX_TRX_FAILED,
-        SELL_INDEX_TRX_FAILED,
-        } from '../actions/types'
+import { call, put, takeEvery } from 'redux-saga/effects'
+import {
+  START_INVEST,
+  START_INVEST_PROCESS,
+  GET_GAS_PRICE,
+  START_SELL_INDEX_PROCESS,
+  START_SELL_INDEX,
+  BUY_INDEX_TRX_FAILED,
+  SELL_INDEX_TRX_FAILED,
+} from '../actions/types';
 
 const ERROR_MSG = 'Network error in determining the gas price';
 
 const getGasPriceAsync = async (stateWeb3) => {
-    const gasPrice = await stateWeb3.getGasPrice()
-    return gasPrice
-}
-
+  const wei = await stateWeb3.getGasPrice();
+  return web3.utils.fromWei(wei, 'gwei');
+};
 
 function* workerStartBuyIndexTokens(action) {
-  //amount:svetTokensAmount,address:svetTokensAddress
   try {
-    const gasPrice = yield call(getGasPriceAsync,action.payload.stateWeb3)
-    yield put({type: GET_GAS_PRICE, payload:{ gasPrice: (gasPrice/100000000) }})
-    yield put({type: START_INVEST_PROCESS, payload: action.payload})
-  // } finally {
-  //   if (yield cancelled()) {
-  //     yield put({type: BUY_INDEX_TRX_END})
-  //   }
-  // }
+    const gasPrice = yield call(getGasPriceAsync, action.payload.stateWeb3);
+    // yield put({ type: GET_GAS_PRICE, payload: { gasPrice: (gasPrice / 100000000) }});
+    yield put({ type: GET_GAS_PRICE, payload: { gasPrice }});
+    yield put({ type: START_INVEST_PROCESS, payload: action.payload });
   } catch {
-    yield put({ type: BUY_INDEX_TRX_FAILED, payload: { hasError: ERROR_MSG } });
+    yield put({ type: BUY_INDEX_TRX_FAILED, payload: { hasError: ERROR_MSG }});
   }
 }
 
 function* workerStartSellIndexTokens(action) {
   try {
-    const gasPrice = yield call(getGasPriceAsync,action.payload.stateWeb3)
-    yield put({type: GET_GAS_PRICE, payload:{ gasPrice: (gasPrice/100000000)  }})
-    yield put({type: START_SELL_INDEX_PROCESS, payload: {}})
+    const gasPrice = yield call(getGasPriceAsync, action.payload.stateWeb3);
+    // yield put({ type: GET_GAS_PRICE, payload: { gasPrice: (gasPrice / 100000000) }});
+    yield put({ type: GET_GAS_PRICE, payload: { gasPrice }});
+    yield put({ type: START_SELL_INDEX_PROCESS, payload: {}});
   } catch {
-    yield put({ type: SELL_INDEX_TRX_FAILED, payload: { hasError: ERROR_MSG } });
+    yield put({ type: SELL_INDEX_TRX_FAILED, payload: { hasError: ERROR_MSG }});
   }
 }
 
 export function* watchStartBuyIndexTokens() {
-    yield takeEvery(START_INVEST, workerStartBuyIndexTokens)
-    yield takeEvery(START_SELL_INDEX, workerStartSellIndexTokens)
+  yield takeEvery(START_INVEST, workerStartBuyIndexTokens);
+  yield takeEvery(START_SELL_INDEX, workerStartSellIndexTokens);
 }
