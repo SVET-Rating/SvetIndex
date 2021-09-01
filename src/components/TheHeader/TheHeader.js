bb0da92c82ed9d798e11b39990b7b65180b7e3c8
Vital Raicheu <rvitaly1978@gmail.com>
2021-09-01 08:41:54 +0300

    feat: add loaders

--- src/components/TheHeader/TheHeader.js
+++ src/components/TheHeader/TheHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {
-  AppBar, IconButton, Toolbar, Typography,
+  AppBar, Box, IconButton, Toolbar, Typography,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import HeaderLogo from '../HeaderLogo/HeaderLogo';
 import HeaderWallet from '../HeaderWallet/HeaderWallet';
+import OrbitLoader from '../loaders/OrbitLoader/OrbitLoader';
 import useStyles from './styles';
 
 const TheHeader = () => {
@@ -18,9 +19,12 @@ const TheHeader = () => {
           <MenuIcon />
         </IconButton>
 
-        <Typography variant="h6" color="inherit" className={classes.grow}>
-          <HeaderLogo />
-        </Typography>
+        <Box className={classes.grow}>
+          <Typography variant="h6" color="inherit">
+            <HeaderLogo />
+          </Typography>
+          <OrbitLoader className={classes.loader} loading={true} />
+        </Box>
 
         <HeaderWallet />
 
