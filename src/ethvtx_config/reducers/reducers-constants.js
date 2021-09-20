71aa0c58b013634575a732d8f8e538faac238563
Vital Raicheu <rvitaly1978@gmail.com>
2021-09-21 00:53:55 +0300

    feat: add stepper steps info about swap

--- src/ethvtx_config/reducers/reducers-constants.js
+++ src/ethvtx_config/reducers/reducers-constants.js
@@ -16,18 +16,3 @@ export const SETTINGS = {
   minDelay: 1,
   maxDelay: 10,
 };
-
-export const BUY_STEPS = [
-  { step: null, label: 'prepare to buy' },
-  { step: SWAP_STATE.start, label: 'start buy' },
-  { step: SWAP_STATE.approve, label: 'approve buy' },
-  { step: SWAP_STATE.end, label: 'end buy' },
-];
-
-export const SELL_STEPS = [
-  { step: null, label: 'prepare to sell' },
-  { step: SWAP_STATE.start, label: 'start sell' },
-  { step: SWAP_STATE.approveToSpend, label: 'approve to spend index' },
-  { step: SWAP_STATE.approve, label: 'approve sell' },
-  { step: SWAP_STATE.end, label: 'end sell' },
-];
